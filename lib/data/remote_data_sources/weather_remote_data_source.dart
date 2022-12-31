04ff92522e4c9aae8536700e8873c694acdd3c5b
Hubert Poździoch <hubikatowice@gmail.com>
2023-01-01 00:04:12 +0100

    Implement Data Source

---
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+class WeatherRemoteDataSource {
+  Future<Map<String, dynamic>?> getWeatherData({
+    required String city,
+  }) async {
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=011d74bc9f6a4fb7b45235711222812&q=$city&aqi=no');
+    return response.data;
+  }
+}
